506f0f65e5571f52286f911ec4b9ded5ffe7e3dc
Chris Schmidt <chrisisbeef@gmail.com>
2025-09-02 13:27:44 -0600

    working on implementing api

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,11 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { listUsers, createUser, updateUser, deleteUser, resetUserPassword } from './api/resource';
-import { PolicyStatement, PolicyDocument, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { listUsers, createUser, updateUser, deleteUser, resetUserPassword } from './api/admin/resource';
+// import { createOrganization, deleteOrganization, getOrganization, inviteUserToOrganization, listOrganizations, updateOrganization } from './api/sigint/Organization.api';
+import { OrganizationAPI } from './api/resource';
+import { Policy, PolicyStatement, PolicyDocument, ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { RestApi, Cors, LambdaIntegration, CognitoUserPoolsAuthorizer, AuthorizationType, MethodOptions } from 'aws-cdk-lib/aws-apigateway';
 
 const backend = defineBackend({
   auth,
@@ -12,6 +15,12 @@ const backend = defineBackend({
   updateUser,
   deleteUser,
   resetUserPassword,
+  createOrganization: OrganizationAPI.create,
+  deleteOrganization: OrganizationAPI.delete,
+  getOrganization: OrganizationAPI.get,
+  inviteUserToOrganization: OrganizationAPI.inviteUser,
+  listOrganizations: OrganizationAPI.list,
+  updateOrganization: OrganizationAPI.update
 });
 
 // Create a policy for Cognito user management
@@ -42,3 +51,56 @@ const cognitoUserManagementPolicy = new ManagedPolicy(backend.auth.resources.use
 
 // Attach the policy to the admin group role
 backend.auth.resources.groups["admin"].role.addManagedPolicy(cognitoUserManagementPolicy);
+
+const api = backend.createStack('pulse-sigint=-api');
+
+// APIs
+const sigintRest = new RestApi(api, 'SigintRestApi', {
+  restApiName: 'Sigint API',
+  description: 'API for Pulse Sigint',
+  deployOptions: {
+    stageName: 'dev',
+  },
+  defaultCorsPreflightOptions: {
+    allowOrigins: Cors.ALL_ORIGINS,
+    allowMethods: Cors.ALL_METHODS,
+    allowHeaders: Cors.DEFAULT_HEADERS,
+  },
+});
+
+// Authorizer
+const apiAuth = new CognitoUserPoolsAuthorizer(api, 'apiAuth', {
+  cognitoUserPools: [backend.auth.resources.userPool]
+});
+
+const apiConfig: MethodOptions = {
+  authorizationType: AuthorizationType.COGNITO,
+  authorizer: apiAuth,
+
+};
+
+// Resource Paths
+const organizationPath = sigintRest.root.addResource('organization');
+
+organizationPath.addMethod('POST', new LambdaIntegration(backend.createOrganization.resources.lambda), apiConfig);
+organizationPath.addMethod('GET', new LambdaIntegration(backend.getOrganization.resources.lambda), apiConfig);
+organizationPath.addMethod('PUT', new LambdaIntegration(backend.updateOrganization.resources.lambda), apiConfig);
+organizationPath.addMethod('DELETE', new LambdaIntegration(backend.deleteOrganization.resources.lambda), apiConfig);
+
+// Proxy Paths
+organizationPath.addProxy({
+  anyMethod: true,
+  defaultIntegration: new LambdaIntegration(backend.listOrganizations.resources.lambda),
+  defaultMethodOptions: apiConfig
+});
+
+const apiRestPolicy = new Policy(api, 'RestApiPolicy', {
+  statements: [
+    new PolicyStatement({
+      actions: ['execute-api:Invoke'],
+      resources: [
+        sigintRest.arnForExecuteApi()
+      ]
+    })
+  ]
+});
